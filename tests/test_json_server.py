0ccb17c9ef42703c12cc2d81eb225e6e57f19f51
viktor_klimanov <viktor.klimanov.2017@mail.ru>
2024-06-26 12:01:44 +0300

    reformat tests

--- tests/test_json_server.py
+++ tests/test_json_server.py
@@ -1,33 +1,32 @@
-from src.json_saver import JSONSaver
 import pytest
+from src.utils import get_vacancy, sort_vacancy
 
 
-@pytest.fixture()
-def json_server():
-    return JSONSaver()
+@pytest.fixture
+def list_vacancy():
+    return [
+    {
+        "name": "Водитель-экспедитор",
+        "url": "https://hh.ru/vacancy/101840292",
+        "salary_from": 110000,
+        "salary_to": 0,
+        "description": "Развоз продукции по Клиентским точкам в Москве и МО. Соблюдение стандартов и правил компании. Ведение документооборота, заполнение накладных, путевых и..."
+    },
+    {
+        "name": "Водитель На Рено Логан",
+        "url": "https://hh.ru/vacancy/102569427",
+        "salary_from": 75000,
+        "salary_to": 115000,
+        "description": "Развоз сотрудников охраны по постам и объектам. Соблюдение правил дорожного движения. Следить за чистотой автомобиля, техническим состоянием."
+    }]
 
 
-def test_sort_vacancy(json_server):
-    assert json_server.sort_vacancy("0 - 100000", 'python') == [
-        {'description': 'Разрабатывать сервисы статистики - построение отчетов по '
-                        'большим объемам данных. Переписывать легаси сервисы на '
-                        'чистовую. Оптимизировать работу сервисов под высокой '
-                        'нагрузкой. ',
-         'name': 'Middle python developer',
-         'salary_from': 0,
-         'salary_to': 0,
-         'url': 'https://hh.ru/vacancy/102211426'},
-        {'description': 'Выполнение прогонов тест-кейсов по проекту. Разработка '
-                        'тестовых артефактов: чек-листы, чит-листы, тест-кейсы, планы '
-                        'тестирования. Фиксация и ведения...',
-         'name': 'QA Engineer (Automation, python)',
-         'salary_from': 0,
-         'salary_to': 0,
-         'url': 'https://hh.ru/vacancy/102647181'}]
+def test_sort_vacancy(list_vacancy):
+    assert sort_vacancy("0 1000000", None, list_vacancy) == []
 
 
-def test_reed_vacancy(json_server):
-    assert json_server.reed_vacancy([
+def test_reed_vacancy():
+    assert get_vacancy([
         {'name': 'Разработчик AI-решений (python + no-code платформы)', 'url': 'https://hh.ru/vacancy/99862545',
          'salary_from': 120000, 'salary_to': 200000,
          'description': 'Кроме того, мы разрабатываем высокоэффективных AI-агентов, которые берут на себя ряд рутинных задач, таких как обработка данных, составление отчетов...'},
